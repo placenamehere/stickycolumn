a01c0717c6538fef8588a696fc9520d8198b7ce4
Chris Casciano <chris@placenamehere.com>
2014-05-14 22:43:40 -0400

    refactor and cleanup, demo update

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,8 @@
 # History
 
+* v0.7.0 May 14, 2014
+  * refactor and cleanup
+  * demo update
 * v0.6.0 May 14, 2014
   * Added ability to pass in a function for calculating contentPadding, in addition to a fixed integer
   * fixed a minor resize related bug
--- dist/jquery.stickycolumn.js
+++ dist/jquery.stickycolumn.js
@@ -1,23 +1,12 @@
 /*
- *  jQuery Stickycolumn - v0.6
+ *  jQuery Stickycolumn - v0.7
  *  A jQuery plugin to do something like position sticky, with constraints
- *  https://github.com/placenamehere/simpleaccordion
+ *  https://github.com/placenamehere/stickycolumn
  *
  *  Made by Chris Casciano
  *  Under MIT License
  */
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
-
-    // undefined is used here as the undefined global variable in ECMAScript 3 is
-    // mutable (ie. it can be changed by someone else). undefined isn't really being
-    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-    // can no longer be modified.
-
-    // window and document are passed through as local variable rather than global
-    // as this (slightly) quickens the resolution process and can be more efficiently
-    // minified (especially when both are regularly referenced in your plugin).
 
     // Create the defaults once
     var pluginName = "stickycolumn",
@@ -30,14 +19,64 @@
     // The actual plugin constructor
     function Plugin ( element, options ) {
       this.element = element;
-      // jQuery has an extend method which merges the contents of two or
-      // more objects, storing the result in the first object. The first object
-      // is generally empty as we don't want to alter the default options for
-      // future instances of the plugin
       this.options = $.extend( {}, defaults, options );
       this._defaults = defaults;
       this._name = pluginName;
+      this.initialized = false;
       this.resize_event;
+      this.doMaths = function($el) {
+        var $parent = $el.parent(),
+            maths = {
+              offsetLeftStart: 0,
+              offsetTopStart: 0,
+              safeH: 9,
+              end: 0
+            };
+
+
+        if (this.initialized) {
+          maths.offsetLeftStart = $el.data(this.options.prefix+"OffsetLeftStart");
+          maths.offsetTopStart = $el.data(this.options.prefix+"OffsetTopStart");
+        } else {
+          this.initialized = true;
+          maths.offsetLeftStart = ($el.offset().left / $(window).width() * 100)+"%";
+          maths.offsetTopStart = $el.offset().top;
+          $el
+            .data(this.options.prefix+"OffsetLeftStart",maths.offsetLeftStart)
+            .data(this.options.prefix+"OffsetTopStart",maths.offsetTopStart);
+        }
+
+        maths.safeH = $el.height() + maths.offsetTopStart;
+        maths.end = ($parent.height() + $parent.offset().top) - $el.height() - this.options.start - this.getContentPadding();
+
+        return maths;
+      };
+      this.updatePosition = function($el,maths) {
+        var winH = $(window).height(),
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (winH < maths.safeH) {
+          $el.removeClass(this.options.prefix+"-active");
+          $el.css("left",0);
+          $el.css("top",0);
+
+        } else {
+          $el.css("left",maths.offsetLeftStart);
+
+          if (scrollTop < this.options.start) {
+            // adjust by scrolltop
+            $el.css("top",maths.offsetTopStart - scrollTop);
+          } else if (scrollTop > maths.end) {
+            // adjust
+            $el.css("top",(maths.offsetTopStart + maths.end) - scrollTop - this.options.start);
+          } else {
+            $el.css("top",maths.offsetTopStart - this.options.start);
+          }
+
+          $el.addClass(this.options.prefix+"-active");
+        }
+
+      };
       this.getContentPadding = function() {
         return (typeof this.options.contentPadding === "function") ? this.options.contentPadding.call() : this.options.contentPadding;
       };
@@ -46,121 +85,25 @@
 
     Plugin.prototype = {
         init: function () {
-            // Place initialization logic here
-            // You already have access to the DOM element and
-            // the options via the instance, e.g. this.element
-            // and this.options
-            // you can add more functions like the one below and
-            // call them like so: this.yourOtherFunction(this.element, this.options).
-
             var _this = this,
                 $el = $(_this.element),
-                $parent = $el.parent(),
-                elH,
-                winH,
-                offsetLeftStart,
-                offsetTopStart,
-                safeH,
-                end = 500,
-                scrollTop;
+                maths;
 
             // initial maths
-
-            elH = $el.height();
-            winH = $(window).height();
-            offsetLeftStart = ($el.data(_this.options.prefix+"OffsetLeftStart") === null) ? $el.data(_this.options.prefix+"OffsetLeftStart") : ($el.offset().left / $(window).width() * 100)+"%";
-            $el.data(_this.options.prefix+"OffsetLeftStart",offsetLeftStart);
-            offsetTopStart = ($el.data(_this.options.prefix+"OffsetTopStart") === null) ? $el.data(_this.options.prefix+"OffsetTopStart") : $el.offset().top;
-            $el.data(_this.options.prefix+"OffsetTopStart",offsetTopStart);
-            safeH = elH + offsetTopStart;
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-
-            // bottom of sticky item should match bottom of container
-            end = ($parent.height() + $parent.offset().top) - elH - _this.options.start - _this.getContentPadding();
-
-            // set current styles accordingly
-            if (winH < safeH) {
-              $el.removeClass(_this.options.prefix+"-active");
-              $el.css("left",0);
-              $el.css("top",0);
-
-            } else {
-              $el.css("left",offsetLeftStart);
-
-              if (scrollTop < _this.options.start) {
-                // adjust by scrolltop
-                $el.css("top",offsetTopStart - scrollTop);
-              } else if (scrollTop > end) {
-                // adjust
-                $el.css("top",(offsetTopStart + end) - scrollTop - _this.options.start);
-              } else {
-                $el.css("top",offsetTopStart - _this.options.start);
-              }
-
-              $el.addClass(_this.options.prefix+"-active");
-            }
-
-
+            maths = _this.doMaths($el);
+            _this.updatePosition($el,maths);
 
             // watch scroll events and reposition
             $(window).on("scroll.stickycolumn",function() {
-              scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-              if (scrollTop < _this.options.start) {
-                // adjust by scrolltop
-                $el.css("top",offsetTopStart - scrollTop);
-              } else if (scrollTop > end) {
-                // adjust
-                $el.css("top",(offsetTopStart + end) - scrollTop - _this.options.start);
-              } else {
-                $el.css("top",offsetTopStart - _this.options.start);
-                // console.log(offsetTopStart - _this.options.start);
-              }
+              _this.updatePosition($el,maths);
             });
-
 
             // watch resize events and reset - f DRY
             $(window).on("resize.stickycolumn",function() {
-              // initial maths
-
-              elH = $el.height();
-              winH = $(window).height();
-              offsetLeftStart = ($el.data(_this.options.prefix+"OffsetLeftStart") === null) ? $el.data(_this.options.prefix+"OffsetLeftStart") : ($el.offset().left / $(window).width() * 100)+"%";
-              // offsetLeftStart = $el.offset().left;
-              $el.data(_this.options.prefix+"OffsetLeftStart",offsetLeftStart);
-              offsetTopStart = ($el.data(_this.options.prefix+"OffsetTopStart") === null) ? $el.data(_this.options.prefix+"OffsetTopStart") : $el.offset().top;
-              $el.data(_this.options.prefix+"OffsetTopStart",offsetTopStart);
-              safeH = elH + offsetTopStart;
-              scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-
-              // bottom of sticky item should match bottom of container
-              end = ($parent.height() + $parent.offset().top) - elH - _this.options.start - _this.getContentPadding();
-
-              // set current styles accordingly
-              if (winH < safeH) {
-                $el.removeClass(_this.options.prefix+"-active");
-                $el.css("left",0);
-                $el.css("top",0);
-
-              } else {
-                $el.css("left",offsetLeftStart);
-
-                if (scrollTop < _this.options.start) {
-                  // adjust by scrolltop
-                  $el.css("top",offsetTopStart - scrollTop);
-                } else if (scrollTop > end) {
-                  // adjust
-                  $el.css("top",(offsetTopStart + end) - scrollTop - _this.options.start);
-                } else {
-                  $el.css("top",offsetTopStart - _this.options.start);
-                }
-
-                $el.addClass(_this.options.prefix+"-active");
-              }
+              // redo initial maths
+              maths = _this.doMaths($el);
+              _this.updatePosition($el,maths);
             });
-
-
         },
         destroy: function() {
             var _this = this,
